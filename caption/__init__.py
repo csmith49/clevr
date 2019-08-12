27b75237fe3d17e54ccd46ff881a8c1a19a43675
Calvin Smith <calvin@Calvins-MacBook-Air.local>
2019-08-12 10:11:19 -0400

    hint refinement

--- caption/__init__.py
+++ caption/__init__.py
@@ -1,1 +1,12 @@
 from .search import greedy, complete
+from .hint import Hint, refine, refineWithEvidence, refineGreedy
+
+def pickBestCaption(captions, target, scene):
+    caption, imgSize = None, len(scene.objects)
+    # pick caption with smallest image containing target
+    for proposal in captions:
+        image = set(proposal.evaluate(scene))
+        if target in image and len(image) < imgSize:
+            caption, imgSize = proposal, len(image)
+    # return the best
+    return caption
